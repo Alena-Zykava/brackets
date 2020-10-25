a174ea250c78d04837007133b651d18c9932edd2
Zykava Alena <zykava.alena@gmail.com>
2020-10-25 21:03:28 +0300

    :fix: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let container = [], pair = {};
+    for(let i = 0; i < bracketsConfig.length; i++) {
+      pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    let Arr = str.split('');
+    for(let i = 0; i < Arr.length; i++) {
+      if(Arr[i] === (container[container.length - 1] || 0)) {
+        container.pop();
+      } else {
+        container.push(pair[Arr[i]]);
+      }
+      
+    }
+    if(container.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
 }
